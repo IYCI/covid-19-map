05bf23e669afb22823071efb5e46f6d9ab210bb0
Jason Feng <jason412389806@hotmail.com>
2020-03-14 18:15:45 -0400

    use JHU data source and support highlight on hover

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { GeoJsonLayer  } from '@deck.gl/layers'; // GeoJsonLayer
 import { StaticMap } from 'react-map-gl';
 import axios from 'axios';
 import chroma from 'chroma-js';
+import { pick } from 'lodash';
 
 import './App.css';
 
@@ -17,32 +18,61 @@ const initialViewState = {
   bearing: 0
 };
 const GEO_JSON_API = 'https://raw.githubusercontent.com/haxzie/covid19-layers-api/master/data/layers/geo.json';
+const GLOBAL_API = 'https://covid19.mathdro.id/api/confirmed';
+const fieldsToFilter = ['provinceState', 'countryRegion', 'lastUpdate', 'lat', 'long', 'confirmed', 'recovered', 'deaths', 'active'];
+const metrics = ['confirmed', 'recovered', 'deaths', 'active'];
 const colorScale = chroma.scale(['lightgreen', 'red']);
 
 function App() {
   const [geoJson, setGeoJson] = useState();
+  // globalData: { <countryCode>: { provinceState, countryRegion, lastUpdate, lat, long, confirmed, recovered, deaths, active } }
+  const [globalData, setGlobalData] = useState({});
   const [toolTipData, setToolTip] = useState(); // { x, y, id, name, data: { confirmed, deaths, recovered } }
+
   useEffect(() => {
-    const getGeoJSON = async () => {
+    const loadData = async () => {
       try {
         const unixTime = new Date().valueOf();
-        const { data } = await axios.get(`${GEO_JSON_API}?ts=${unixTime}`);
-        setGeoJson(data);
+        const [{ data: geoJSONData }, { data: globalDataList }] = await Promise.all([
+          axios.get(`${GEO_JSON_API}?ts=${unixTime}`),
+          axios.get(GLOBAL_API),
+        ]);
+        
+        const globalDataMap = {};
+        globalDataList.forEach(d => {
+          let country = globalDataMap[d.iso3];
+          if (!country) {
+            globalDataMap[d.iso3] = pick(d, ['countryRegion', 'lastUpdate', 'confirmed', 'recovered', 'deaths', 'active']);
+            globalDataMap[d.iso3].provinces = { [d.provinceState]: pick(d, fieldsToFilter) };
+          } else {
+            metrics.forEach(m => {
+              country[m] += d[m];
+            });
+            if (d.lastUpdate > country.lastUpdate) {
+              country.lastUpdate = d.lastUpdate;
+            }
+            country.provinces[d.provinceState] = pick(d, fieldsToFilter);
+          }
+        })
+        window.globalDataMap = globalDataMap; // for debugging purpose
+        console.log(globalDataMap)
+        setGlobalData(globalDataMap);
+        setGeoJson(geoJSONData);
       } catch (err) {
         console.error(err);
       }
     };
-    getGeoJSON();
+    loadData();
   }, []);
 
   const _renderTooltip = () => {
     if (!toolTipData) {
       return null;
     }
-    const { x, y, name, data: { confirmed, deaths, recovered } } = toolTipData;
+    const { x, y, name, id, data: { confirmed, deaths, recovered } } = toolTipData;
     return (
       <div style={{ position: 'absolute', zIndex: 1, pointerEvents: 'none', left: x, top: y, backgroundColor: 'white', padding: '1rem', borderRadius: '4px' }}>
-        <h2 style={{ margin: '0 0 1rem 0' }}>{name}</h2>
+        <h2 style={{ margin: '0 0 1rem 0' }}>{name} - {id}</h2>
         <div>confirmed: {confirmed}</div>
         <div>deaths: {deaths}</div>
         <div>recovered: {recovered}</div>
@@ -56,9 +86,13 @@ function App() {
     new GeoJsonLayer ({
       id: 'countries-geo-json',
       data: geoJson,
+      highlightColor: [0, 0, 0, 50],
+      autoHighlight: true,
       getFillColor: d => {
-        if (d.properties.data.confirmed > 0) {
-          return colorScale(d.properties.data.confirmed / 7000).rgb();
+        const metric = 'confirmed'; // TODO: make this switchable
+        if (globalData[d.id] && globalData[d.id][metric] > 0) {
+          const metricValue = globalData[d.id][metric];
+          return colorScale(metricValue / 7000).rgb();
         }
         return [0, 0, 0, 0];
       },
@@ -74,10 +108,13 @@ function App() {
           const { x, y, object: { id, properties } } = data;
           setToolTip({ ...properties, id, x, y })
         } else {
-          console.log('asdasd')
           setToolTip(null);
         }
       },
+      onClick: (data) => {
+        console.log('clicked:', data)
+        console.log('country data:', globalData[data.object.id]);
+      }
     })
   ];
 
