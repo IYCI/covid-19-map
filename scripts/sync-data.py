1eee7cac303710cc9eee7acc96389ef879f6be96
19506jk <pengdanny23@gmail.com>
2020-03-15 16:44:41 -0400

    Add scripts

---
+++ scripts/sync-data.py
@@ -0,0 +1,153 @@
+import urllib.request
+import urllib.parse
+import csv
+import json
+from datetime import datetime
+
+with open('./country-mapping.json') as f:
+  countryMapping = json.load(f)
+
+CONFIRMED_DATA_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+DEATHS_DATA_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+RECOVERED_DATA_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
+
+def convertDateString(original):
+  return datetime.strptime(original, '%m/%d/%y').strftime('%y/%m/%d')
+
+"""
+Creates initial country data object with CONFIRMED_DATA_URL
+Modifies countryData global variable to the following format:
+{
+  <country-iso3-code>: {
+    countryName,
+    timeSeries: {
+      <date1>: { c, d, r }
+    },
+    provinces: {
+      <provinceName1>: {
+        timeSeries: {
+          <date>: { c, d, r }
+        }
+      },
+      <provinceName2>: {
+        timeSeries: {
+          <date>: { c, d, r }
+        }
+      }
+    }
+  }
+}
+
+Where:
+c - Confirmed
+d - Deaths
+r - Recovered
+"""
+def create_inital_data():
+  try:
+    response = urllib.request.urlopen(CONFIRMED_DATA_URL)
+  except urllib.error.HTTPError as error:
+    print(error.code)
+    print(error.read())
+
+  parsedData = response.read().decode('utf-8').splitlines()
+
+  csvReader = csv.reader(parsedData)
+  headers = []
+  countryCode = ''
+
+  for row in csvReader:
+    if len(headers) == 0:
+      headers = row
+      continue
+
+    countryName = row[1]
+    if countryName not in countryMapping:
+      print(f'{countryName} is not found in country mapping!')
+      break
+    countryCode = countryMapping.get(countryName)
+
+    timeSeries = {}
+    for i in range(4, len(headers)):
+      date = convertDateString(headers[i])
+      timeSeries[date] = {
+          'c': int(row[i]),
+          'd': '',
+          'r': '',
+      }
+
+    coordinates = {
+      'lat': row[2],
+      'long': row[3]
+    }
+
+    countryObject = countryData.get(countryCode)
+    if countryObject == None:
+      countryObject = {
+        'countryName': countryName,
+        'provinces': {}
+      }
+
+    provinceName = row[0]
+    if len(provinceName) > 0:
+      countryObject['provinces'][provinceName] = {
+        'timeSeries': timeSeries,
+        'coordinates': coordinates
+      }
+    else:
+      countryObject['coordinates'] = coordinates
+      countryObject['timeSeries'] = timeSeries
+    countryData[countryMapping[countryName]] = countryObject
+
+"""
+Modifies existing countryData global variable and update the provided field
+with data from url
+ex. appendMetricData(RECOVERED_DATA_URL, 'r') will update the r (recovered) field in the
+timeSeries with data from RECOVERED_DATA_URL
+"""
+def appendMetricData(url, field):
+  try:
+    response = urllib.request.urlopen(url)
+  except urllib.error.HTTPError as error:
+    print(error.code)
+    print(error.read())
+    return
+
+  parsedData = response.read().decode('utf-8').splitlines()
+  csvReader = csv.reader(parsedData)
+  headers = []
+
+  for row in csvReader:
+    if len(headers) == 0:
+      headers = row
+      continue
+
+    countryName = row[1]
+    if countryName not in countryMapping:
+      print(f'{countryName} is not found in country mapping!')
+      break
+
+    countryCode = countryMapping.get(countryName)
+    countryObj = countryData.get(countryCode)
+    provinceName = row[0]
+
+    if len(provinceName) > 0:
+      timeSeries = countryObj['provinces'][provinceName]['timeSeries']
+    else:
+      timeSeries = countryObj.get('timeSeries')
+
+    for i in range(4, len(headers)):
+      date = convertDateString(headers[i])
+      timeSeries[date][field] = int(row[i])
+
+countryData = {}
+
+def main():
+  create_inital_data()
+  appendMetricData(DEATHS_DATA_URL, 'd')
+  appendMetricData(RECOVERED_DATA_URL, 'r')
+  with open('country-data.json', 'w') as f:
+    json.dump(countryData, f)
+
+if __name__ == '__main__':
+    main()
