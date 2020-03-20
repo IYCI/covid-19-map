197f0660871d518096966d0ed48b8dc67874648c
19506jk <pengdanny23@gmail.com>
2020-03-19 23:34:59 -0400

    WIP

--- scripts/create-country-mapping.py
+++ scripts/create-country-mapping.py
@@ -24,18 +24,39 @@ countryMapping = {
   'Congo (Kinshasa)': 'CG',
   'Congo (Brazzaville)': 'CD',
   'occupied Palestinian territory': 'PSE',
-  'Gambia, The': 'GMB'
+  'Gambia, The': 'GMB',
+  'Bahamas, The': 'BS'
 }
 name = ''
+populationMap = {}
 
+next(csvReader) # Skip header
 for row in csvReader:
-  if lineCount > 0:
     name = row[1]
 
     if name not in countryMapping:
-      code = pycountry.countries.search_fuzzy(name)[0].alpha_3
-      countryMapping[row[1]] = code
-  lineCount += 1
+      try:
+        code = pycountry.countries.search_fuzzy(name)[0].alpha_3
+        countryMapping[row[1]] = code
+      except:
+        print(f'Failed to look up iso code for {name}')
+
+    if len(row[0]) > 0 and ',' not in row[0]:
+      populationMap[row[0]] = ''
+    else:
+      populationMap[code] = ''
+
+with open('population.csv', 'r') as f:
+  reader = csv.reader(f)
+  next(reader)
+  for entry in reader:
+    if entry[1] in populationMap:
+      filtered = list(filter(lambda x: len(x) > 0, entry))
+      populationMap[entry[1]] = int(filtered[-1])
+
 
 with open('country-mapping.json', 'w') as jsonFile:
   json.dump(countryMapping, jsonFile)
+
+with open('population-mapping.json', 'w') as jsonFile:
+  json.dump(populationMap, jsonFile)
