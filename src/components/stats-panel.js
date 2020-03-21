e98806711c5ac674c369f80885d7cbac9db0be01
Jason Feng <jason412389806@hotmail.com>
2020-03-21 01:12:00 -0400

    show country level in stats pne;

--- src/components/stats-panel.js
+++ src/components/stats-panel.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Statistic, Card, List, Button } from 'antd';
+import { Statistic, Card, List, Button, Breadcrumb } from 'antd';
 import { LineChartOutlined, CloseOutlined } from '@ant-design/icons';
-import numeral from 'numeral'
+import numeral from 'numeral';
+import { isEmpty } from 'lodash';
 
 import './stats-panel.css';
 
@@ -9,34 +10,53 @@ import './stats-panel.css';
 export default function StatsPanel ({ globalData }) {
   const [minimized, setMinimized] = useState(false);
   const [metric, setMetric] = useState('active');
+  const [countryCode, setCountry] = useState(); // iso3
 
-  const total = {
+  let total = {
     active: 0,
     confirmed: 0,
     recovered: 0,
     deaths: 0,
   }
-
-  let countryList = Object.entries(globalData).map(([iso3, data]) => {
-    total.confirmed += data.confirmed;
-    total.active += data.active;
-    total.recovered += data.recovered;
-    total.deaths += data.deaths;
-    return { ...data, iso3 };
-  }).sort((c1, c2) => c2[metric] - c1[metric]);
   const metricList = Object.keys(total);
 
+  let areaList = [];
+  if (countryCode && globalData[countryCode]) { // country
+    total = globalData[countryCode];
+    if (!isEmpty(globalData[countryCode].provinces)) {
+      areaList = Object.entries(globalData[countryCode].provinces).map(([areaCode, data]) => {
+        return { ...data, areaCode, name: data.provinceState || 'N/A' };
+      })
+    }
+  } else { // global
+    areaList = Object.entries(globalData).map(([areaCode, data]) => {
+      total.confirmed += data.confirmed;
+      total.active += data.active;
+      total.recovered += data.recovered;
+      total.deaths += data.deaths;
+      return { ...data, areaCode, name: data.countryRegion };
+    })
+  }
+  areaList.sort((c1, c2) => c2[metric] - c1[metric]);
+  console.log(areaList);
+  
+  const resetPanel = () => {
+    setMinimized(false);
+    setCountry(null);
+  }
+  
   if (minimized) {
     return (
       <Button
-        onClick={() => setMinimized(false)}
-        className="minimized-button"
+        onClick={() => resetPanel()}
+        className='minimized-button'
         icon={<LineChartOutlined />}
         size='large'
       />
     )
   }
 
+  const listItemClasses = `stats-list-item ${countryCode ? '' : 'clickable'}`;
   const collapseBtn = (
     <Button
       onClick={() => setMinimized(true)}
@@ -46,7 +66,13 @@ export default function StatsPanel ({ globalData }) {
     </Button>
   );
   return (
-    <Card title="Global Stats" className='stats-panel' extra={collapseBtn}>
+    <Card
+      title={(
+        <CardTitle country={countryCode && globalData[countryCode].countryRegion} setCountry={setCountry} />
+      )}
+      className='stats-panel'
+      extra={collapseBtn}
+    >
       <div className='stats-group'>
         {metricList.map(m => (
           <div className='stats-wrapper' onClick={() => setMetric(m)} key={m}>
@@ -58,16 +84,38 @@ export default function StatsPanel ({ globalData }) {
           </div>
         ))}
       </div>
-      <List
-        className='stats-list'
-        dataSource={countryList}
-        renderItem={country => (
-          <List.Item className="stats-list-item">
-            {country.countryRegion}
-            <span>{numeral(country[metric]).format('0,0')}</span>
-          </List.Item>
-        )}
-      />
+      {areaList && areaList.length > 0 && (
+        <List
+          className='stats-list'
+          dataSource={areaList}
+          renderItem={area => (
+            <List.Item
+              className={listItemClasses} onClick={() => !countryCode && setCountry(area.areaCode)}>
+              {area.name}
+              <span>{numeral(area[metric]).format('0,0')}</span>
+            </List.Item>
+          )}
+        />
+      )}
     </Card>
+  )
+}
+
+const CardTitle = ({ country, setCountry }) => {
+  let title = 'Global Stats';
+  if (country) {
+    title = country;
+  }
+
+  return (
+    <div>
+      { country && (
+        <Breadcrumb separator=''>
+          <Breadcrumb.Item className='breadcrumb-item' onClick={() => setCountry(null)}>Global Stats</Breadcrumb.Item>
+          <Breadcrumb.Separator>></Breadcrumb.Separator>
+        </Breadcrumb>
+      )}
+      <h3 style={{marginBottom: '0px'}}>{title}</h3>
+    </div>
   )
 }
